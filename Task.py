26f7cbc0a5344e12095ccb454d8c96d3d334f596
metsl <39564160+metsl@users.noreply.github.com>
2019-12-14 19:59:28 +0300

    Add files via upload

--- Task.py
+++ Task.py
@@ -1,64 +1,66 @@
 import re
-import fileinput
-import sys
-import os
-import argparse
+
+file: str = input("enter filename:")
+pattern: str = input("enter text to be searched:")
 
 
-# inputing data from file (located in the same folder as Python)
-# i don't know how to call this function correctly
+class InputOutput:
 
-class InputOutput():
+    def in_data():
+        """""
+        input data from file
+        """
+        data = open(file, "r")
+        result = ""
+        for line in data:
+            if re.match(f"(.+) {pattern} (.+)", line):
+                result = result + line
+        return result
 
-    def __init__(self, pattern, file, text):
-        self.pattern = pattern
+    def __init__(self, file, pattern):
         self.file = file
-        self.text = text
+        self.pattern = pattern
 
-    file = input("enter filename:")
-    text = input("enter text to be searched:")
+    # asking user for inputting any of 3 options: underscoring, highlighting and generate machine readable output
+    print("Available output options:\nEnter -u ( --underscore )\nEnter -c ( --color highlighting )\nEnter -m ( "
+          "--machine readable output \nEnter -x for standard output")
 
-    pattern = open(file, "r")
+    # print("{:s}\n{:s}\n".format(result, len(result) * "^"))   #underlines with ^ the whole line.
 
-    for line in pattern:
-        if re.match('(.+) ' + text + ' (.+)', line):
-            print(line)
+    def get_output_option(self, pattern, result):
 
+        """""
+        underlining matching text (not with ^) on -u
+        Highlighting matching text on -c
+        outputting machine readable output on -m
+        outputting regular text on -x
+        """
+        choice = input("Enter -u ( -- for underscore )\nEnter -c ( --color highlighting )\nEnter -m ( --machine "
+                       "readable output \nEnter -x for standard output")
+        if choice == 'u':
+            formattedtext = []
+            for t in result.lower().split():
+                if t == pattern:
+                    formattedtext.append("\\033[4m" + t + "\033[0m")
+                else:
+                    formattedtext.append(t)
 
-# asking user for inputing any of 3 options: underscoring, highliting and generate machine readable output
-print("Available output options:\nEnter -u ( --underscore )\nEnter -c ( --color highliting )\nEnter -m ( --machine readable output \nEnter -x for standard output")
+            print(" ".join(formattedtext))
 
+        elif choice == 'c':
+            formattedtext  = []
+            for t in result.lower().split():
+                if t == pattern:
+                    formattedtext.append("\033[92m" + t + "\033[0m")
+                else:
+                    formattedtext.append(t)
 
-# underlining matching text (not with ^)
-# print("{:s}\n{:s}\n".format(line, len(line) * "^"))   #underlines with ^ the whole line.
-def get_output_option(self):
-    if choise == u:
-        formattedText = []
-        for t in line.lower().split():
-            if t == text:
-                formattedText.append("\\033[4m" + t + "\033[0m")
-            else:
-                formattedText.append(t)
-
-        print(" ".join(formattedText))
-
-
-# Highliting matching text
-    elif choise == c:
-        formattedText = []
-        for t in line.lower().split():
-            if t == text:
-                formattedText.append("\033[92m" + t + "\033[0m")
-            else:
-                formattedText.append(t)
-
-        print(" ".join(formattedText))
+            print(" ".join(formattedtext))
         # print( "\033[92m {}\033[00m" .format(line))  #highlights the whole line, not pattern
 
-    elif choise == m:
-        pass  # machine readable input
+        elif choice == 'm':
+            pass  # machine readable input
 
-    # outputing regular text
-    elif choise == x:
-        print(line)
-    return 0
+        elif choice == 'x':
+            print(result)
+        return 0
