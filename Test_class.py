35cdd5c47d0c3a710b53eb255ed3072772bd9b6f
metsl <39564160+metsl@users.noreply.github.com>
2019-12-15 21:27:12 +0200

    Add files via upload

--- Test_class.py
+++ Test_class.py
@@ -12,14 +12,9 @@ class Word:
 
         if path is None:
             path = self.file_path
-        with open(path, 'r') as file:
-            for line in file:
-                try:
-                    word = re.search(pattern, line)
-                    if word is not None:
-                        word.group(0)
-                    else:
-                        print('No word found or typo')
-                except Exception:
-                    print('Cannot open file or find word')
-        return word.group(0)
+        with open( path, 'r' ) as file:
+            for line in file.readlines():
+                if re.findall( pattern, line, re.I ):
+                    print(line)
+        return line
+
