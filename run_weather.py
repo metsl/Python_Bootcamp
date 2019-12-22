18d9e164c24f7648083af665021c0d66e02c7f65
metsl <39564160+metsl@users.noreply.github.com>
2019-12-22 11:29:44 +0200

    Add files via upload

--- run_weather.py
+++ run_weather.py
@@ -2,21 +2,23 @@ import tkinter as tk
 from tkinter import filedialog
 import csv
 
-
 def main():
     folder = tk.Tk()
     folder.withdraw()
     file_path_string = filedialog.askopenfilename()
     with open(file_path_string) as f:
         content = f.read()
+    print(content)
+
+
     output_file = "city_info.csv"
-    fieldnames = ['city', 'max temp', 'min temp', 'sunset']
-    writer = csv.DictWriter(output_file, fieldnames=fieldnames)
-
-    writer.writeheader()
-    writer.writerow({'max temp': '25', 'min temp': '-10', 'sunset': '21:00'})
-    writer.writerow({'max temp': '29', 'min temp': '-3', 'sunset': '22:00'})
-     writer.writerow({'max temp': '24', 'min temp': '3', 'sunset': '20:00'})
-
+    city_info = ('max temp: 25, min temp: -10, sunset: 21:00',
+                 'max temp: 29, min temp: -3, sunset: 22:00',
+                 'max temp: 24, min temp: 3, sunset: 20:00')
+    with open(output_file, 'w') as f:
+        w = csv.DictWriter(f, fieldnames = ['city', 'city_info'], delimiter=" ")
+        w.writeheader()
+        for i in range(0, len(city_info)):
+             writer.writerow({'city': content[i], 'city_info': city_info[i]}
 
 main()
